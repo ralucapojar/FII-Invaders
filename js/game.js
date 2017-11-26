23ef233ce011d35959741d157ff8c8ef170a2483
raluca <pojar_ralucamihaela@gmail.com>
2017-11-26 23:55:12 +0200

    main game html and move

---
+++ js/game.js
@@ -0,0 +1,23 @@
+
+// function doMoveLeft() {
+// 	var monsterBox = document.getElementById("monstri");
+// 	monsterBox.style.marginLeft  = parseInt(monsterBox.style.marginLeft ) + 1 + 'px';
+// 	setTimeout(doMoveLeft,40); 
+
+// }
+
+// function doMoveRight() {
+// 	var monsterBox = document.getElementById("monstri");
+// 	monsterBox.style.marginRight  = parseInt(monsterBox.style.marginRight ) - 1 + 'px';
+// 	setTimeout(doMoveRight,40); 
+// }
+
+// function init() {
+//   monsterBox = document.getElementById("monstri");
+//   monsterBox.style.marginLeft  = '0px'; // set its initial position to 0px
+//   doMoveLeft(); // start animating
+
+// }
+
+
+// window.onload = init;
