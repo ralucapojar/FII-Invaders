f882476ce42c728d0ef1b4c36809254e789d0cc9
lupu.vlad <vlad.lupu12@yahoo.com>
2018-01-10 20:37:07 +0200

    update invares

--- js/nivel1.js
+++ js/nivel1.js
@@ -36,20 +36,17 @@
     var invaderBulletFrequency = INVADER_START_BULLET_FREQUENCY;
     loadImage('ship', '../img/student1.png');
     loadImage('invader1', '../img/monster1.png');
-    
-    loadImage('invader2', '../img/monster1.png');
-    
-    loadImage('invader3', '../img/monster1.png');
-    
+  	loadImage('invader2', '../img/monster1.png');
+  	loadImage('invader3', '../img/monster1.png');
+
     loadImage('bullet', '../img/bomba1.png');
     loadImage('base', '../img/licenta.png');
     window.onload = function() {
         canvas = document.getElementById('canvas');
         context = canvas.getContext('2d');
         
-        playerShip = new playerShip();
+       
         setupInvaders();
-        setupBases();
         canvas.addEventListener('keydown', function(e) {
             handleKeypress(e);
         });
@@ -79,27 +76,8 @@
             startY += INVADER_MAX_HEIGHT + INVADER_VERT_GAP;
         }
     };
-    function setupBases() {
-        var divider = PLAYER_BASES * 2;
-        var xIncrement = canvas.width / divider;
-        var startX = xIncrement;
-        var addBase = true;
-        for (var baseNum = 0; baseNum < divider; baseNum++) {
-            if (addBase) {
-                var newBase = new base(startX, canvas.height - (200 * SCALE_FACTOR));
-                bases.push(newBase);
-            }
-            startX += xIncrement;
-            addBase = !addBase;
-        }
-    };
-    function drawBases() {
-        if (bases.length) {
-            bases.forEach(function(baseIcon) {
-                baseIcon.draw();
-            })
-        }
-    };
+
+   
     function drawInvaders() {
         if (invaders.length > 0) {
             invaders.forEach(function(invaderIcon) {
@@ -109,6 +87,7 @@
             });
         }
     };
+    
     function invaderChangeDirection() {
         if (invaderMoveDirection === invaderDirection.right) {
             invaderMoveDirection = invaderDirection.left;
@@ -116,41 +95,11 @@
             invaderMoveDirection = invaderDirection.right;
         }
     };
-    function drawBullets() {
-        if (bullets.length > 0) {
-            bullets.forEach(function(bullet) {
-                bullet.draw();
-                bullet.move();
-            });
-        }
-    }
-    function handleKeypress(e) {
-        e = e || event; // to deal with IE
-        keyMap[e.keyCode] = e.type == 'keydown';
-    };
-    function playerMovement() {
-        if (keyMap[37]) {
-            playerShip.moveLeft();
-        }
-        if (keyMap[39]) {
-            playerShip.moveRight();
-        }
-        if (keyMap[32]) {
-            fireBullet(bulletType.player, playerShip);
-        }
-    };
-    function fireBullet(type, owner) {
-        var newBullet = new bullet(type, owner);
-        bullets.push(newBullet);
-    };
-    function redraw() {
+   
+   function redraw() {
         context.fillStyle = 'black';
         context.fillRect(0, 0, canvas.width, canvas.height);
-        playerShip.draw();
         drawInvaders();
-        drawBullets();
-        drawBases();
-        playerMovement();
     };
     function loadImage(imageName, imageFile) {
         var img = new Image();
@@ -159,50 +108,21 @@
         };
         img.src = imageFile;
     };
-    function removeBullet(bullet) {
-        var pos = bullets.indexOf(bullet);
-        if (pos > -1) {
-            bullets.splice(pos, 1);
-        }
-    };
-    function playerShip() {
-        this.image = imageCache.ship;
-        this.width = this.image.width * SCALE_FACTOR;
-        this.height = this.image.height * SCALE_FACTOR;
-        this.x = (canvas.width - this.width) / 2;
-        this.y = canvas.height - this.height;
-        this.moveLeft = function () {
-            if (this.x - PLAYER_SHIP_SPEED > 0) {
-                this.x -= PLAYER_SHIP_SPEED;
-            }
-        }
-        this.moveRight = function () {
-            if (this.x + this.width + PLAYER_SHIP_SPEED < canvas.width) {
-                this.x += PLAYER_SHIP_SPEED;
-            }
-        }
-        this.draw = function() {
-            context.drawImage(this.image, this.x, this.y, this.width, this.height);
-        };
-    };
+    
     function invader(type, startX, startY) {
         this.lastMoveTick = this.lastBulletTick = (new Date()).getTime();
         this.type = type;
         this.firing = false;
         this.direction = invaderMoveDirection;
-        this.imageFrames = [
-            imageCache['invader' + this.type + '1'],
-            imageCache['invader' + this.type + '2']
-        ];
-        this.frame = 0;
-        this.image = this.imageFrames[this.frame];
+        this.image = imageCache['invader' + this.type];
         this.width = this.image.width * SCALE_FACTOR;
         this.height = this.image.height * SCALE_FACTOR;
         this.x = startX - (INVADER_MAX_WIDTH / 2);
         this.y = startY - (INVADER_MAX_HEIGHT / 2);
         this.count = 0;
+       
         this.draw = function() {
-            this.image = this.imageFrames[this.frame];
+            this.image = imageCache['invader' + this.type];
             context.drawImage(this.image, this.x, this.y, this.width, this.height);
         };
         this.move = function() {
@@ -216,11 +136,7 @@
                 if (this.direction === invaderDirection.right) {
                     if (this.x + INVADER_MOVE_SPEED < canvas.width) {
                         this.x += INVADER_MOVE_SPEED;
-                        if (this.frame === 0) {
-                            this.frame = 1;
-                        } else {
-                            this.frame = 0;
-                        }
+                  
                     } else {
                         // invaderMoveDirection = invaderDirection.left;
                         // this.direction = invaderMoveDirection;
@@ -229,11 +145,7 @@
                 } else {
                     if (this.x - INVADER_MOVE_SPEED > 0) {
                         this.x -= INVADER_MOVE_SPEED;
-                        if (this.frame === 0) {
-                            this.frame = 1;
-                        } else {
-                            this.frame = 0;
-                        }
+                      
                     } else {
                         // invaderMoveDirection = invaderDirection.right;
                         // this.direction = invaderMoveDirection;
@@ -252,51 +164,4 @@
             return false;
         };
     };
-    function bullet(type, owner) {
-        var getColourAtPoint = function(x, y) {
-            var p = context.getImageData(x, y, 1, 1).data; 
-            // var hex = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
-            return p[0] + p[1] + p[2];
-        };
-        this.type = type;
-        this.image = imageCache.bullet;
-        this.width = this.image.width * SCALE_FACTOR;
-        this.height = this.image.height * SCALE_FACTOR;
-        this.x = owner.x + ((owner.width - this.width) / 2);
-        if (this.type === bulletType.player) {
-            this.y = owner.y - this.height;
-        } else {
-            this.y = owner.y + owner.height;
-        }
-        this.draw = function() {
-            context.drawImage(this.image, this.x, this.y, this.width, this.height);
-        };
-        this.move = function() {
-            if (this.type === bulletType.player) {
-                this.y -= PLAYER_BULLET_SPEED;
-                if (this.y < 0) {
-                    removeBullet(this);
-                }
-                // var c = getColourAtPoint(this.x + (this.width / 2), this.y - 1);
-                // if (c > 0) {
-                //     console.log(c);
-                // }
-            }
-            if (this.type === bulletType.invader) {
-                this.y += INVADER_BULLET_SPEED;
-                if (this.y > canvas.height) {
-                    removeBullet(this);
-                }
-            }
-        }
-    };
-    function base(x, y) {
-        this.image = imageCache.base;
-        this.width = this.image.width * SCALE_FACTOR;
-        this.height = this.image.height * SCALE_FACTOR;
-        this.x = (x - this.width / 2);
-        this.y = (y - this.height / 2);
-        this.draw = function () {
-            context.drawImage(this.image, this.x, this.y, this.width, this.height);            
-        };
-    };
+ 
