80b575a59af79e2a46f01cd9c88393d02053894e
raluca <pojar_ralucamihaela@gmail.com>
2018-01-28 18:21:04 +0200

    absolvit high scores

---
+++ js/absolvit.js
@@ -0,0 +1,30 @@
+    var objPlayer;
+    var sortable = [];
+   
+
+    objPlayer = Object.keys(localStorage).reduce(function(obj, str) { 
+        obj[str] = localStorage.getItem(str); 
+        return obj
+    }, {});
+
+    for (var player in objPlayer) {
+        sortable.push([player, objPlayer[player]]);
+    }
+
+    sortable.sort(function(a, b) {
+        return b[1] - a[1];
+    }) ;
+
+    function printHighScore(){
+    var index = 1 ;
+     for (var player in sortable) {
+        var h1 = document.createElement("h3");   
+        var text = "" + index  + ". " + sortable[player][0] + " : " + sortable[player][1];        
+        var textnode = document.createTextNode(text);         
+        h1.appendChild(textnode);                         
+        document.getElementById("high").appendChild(h1);  
+        index++;
+     }
+}
+
+printHighScore();
